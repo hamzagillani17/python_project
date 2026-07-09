89f0709eeba1748f6e8f9e18abc8ac9c4ce370a1
hg_python <hamzagillani17@gmail.com>
2026-07-09 21:49:11 +0200

    done all opreators assingment

--- 04_Operators/assignment_operators.py
+++ 04_Operators/assignment_operators.py
@@ -18,7 +18,7 @@ print("Augmented addition of int and int")
 a+=b # equivalent to a=a+b
 print ("a =",a, "type(a):", type(a))
 b+=1 #equivalent to b=b+1
-print("\nb = ",b)
+print("b = ",b)
 
 a=10
 b=5.5
@@ -37,8 +37,109 @@ print(b,"\ntype", type(b))
 
 print("\n-------Augmented Subtraction Operator (-=)---------")
 
+a = 10
+b = 5
+
+print("Augmented Subtraction of int and int")
+a-=b #equivent to a = a-b
+print(a," type: ",type(a))
+
+a=10
+b=5.5
+print ("Augmented subtraction of int and float")
+a-=b #equivalent to a=a-b
+print ("a=",a, "type(a):", type(a))
+
+a=10.50
+b=5+6j
+print ("Augmented subtraction of float and complex")
+a-=b #equivalent to a=a-b
+print ("a=",a, "type(a):", type(a))
+
+print("\n======Augmented Multiplication Operator (*=)=======")
+
+a=10
+b=5
+print ("Augmented multiplication of int and int")
+a*=b #equivalent to a=a*b
+print ("a=",a, "type(a):", type(a))
+
+a=10
+b=5.5
+print ("Augmented multiplication of int and float")
+a*=b #equivalent to a=a*b
+print ("a=",a, "type(a):", type(a))
+
+a=6+4j
+b=3+2j
+print ("Augmented multiplication of complex and complex")
+a*=b #equivalent to a=a*b
+print ("a=",a, "type(a):", type(a))
+
+print("\n=======Augmented Division Operator (/=)=====")
+
+a=10
+b=5
+print ("Augmented division of int and int")
+a/=b #equivalent to a=a/b
+print ("a=",a, "type(a):", type(a))
+
+a=10
+b=5.5
+print ("Augmented division of int and float")
+a/=b #equivalent to a=a/b
+print ("a=",a, "type(a):", type(a))
+
+a=6+4j
+b=3+2j
+print ("Augmented division of complex and complex")
+a/=b #equivalent to a=a/b
+print ("a=",a, "type(a):", type(a))
 
 
+print("\n=========Augmented Modulus Operator (%=)==========")
 
+a=10
+b=5
+print ("Augmented modulus operator with int and int")
+a%=b #equivalent to a=a%b
+print ("a=",a, "type(a):", type(a))
 
+a=10
+b=5.5
+print ("Augmented modulus operator with int and float")
+a%=b #equivalent to a=a%b
+print ("a=",a, "type(a):", type(a))
 
+print("=======Augmented Exponent Operator (**=)======")
+a=10
+b=5
+print ("Augmented exponent operator with int and int")
+a**=b #equivalent to a=a**b
+print ("a=",a, "type(a):", type(a))
+
+a=10
+b=5.5
+print ("Augmented exponent operator with int and float")
+a**=b #equivalent to a=a**b
+print ("a=",a, "type(a):", type(a))
+
+a=6+4j
+b=3+2j
+print ("Augmented exponent operator with complex and complex")
+a**=b #equivalent to a=a**b
+print ("a=",a, "type(a):", type(a))
+
+print("\n=======Augmented Floor division Operator (//========")
+
+a=10
+b=5
+print ("Augmented floor division operator with int and int")
+a//=b #equivalent to a=a//b
+print ("a=",a, "type(a):", type(a))
+
+a=10
+b=5.5
+print ("Augmented floor division operator with int and float")
+a//=b #equivalent to a=a//b
+print ("a=",a, "type(a):", type(a))
