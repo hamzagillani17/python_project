09c8b838f1971cafe6f599f9e11f7beb939f0c49
hg_python <hamzagillani17@gmail.com>
2026-07-09 21:36:01 +0200

    update op

--- 04_Operators/assignment_operators.py
+++ 04_Operators/assignment_operators.py
@@ -1,4 +1,43 @@
 print("\nAssignment Operators")
+'''
+a += b: 40
+a -= b: 28
+a *= b: 204
+a /= b: 5.666666666666667
+a %= b: 4
+a **= b: 1544804416
+a //= b: 5
+'''
+
+print("\nAugmented Addition Operator (+=)")
+
+a=10
+b=5
+
+print("Augmented addition of int and int")
+a+=b # equivalent to a=a+b
+print ("a =",a, "type(a):", type(a))
+b+=1 #equivalent to b=b+1
+print("\nb = ",b)
+
+a=10
+b=5.5
+
+print ("Augmented addition of int and float")
+a+=b  # equivalent to a=a+b
+print ("a=",a, "type(a):", type(a))
+
+a=10.50
+b=5+6j
+print ("Augmented addition of float and complex")
+a+=b #equivalent to a=a+b
+print ("a=",a, "type(a):", type(a))
+b+=1
+print(b,"\ntype", type(b))
+
+print("\n-------Augmented Subtraction Operator (-=)---------")
+
+
 
 
 
