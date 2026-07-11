ca7d4e05b5ef1272f91140c065687cd3b0eefd91
hg_python <hamzagillani17@gmail.com>
2026-07-12 00:49:26 +0200

    done

--- 04_Operators/identity_operators.py
+++ 04_Operators/identity_operators.py
@@ -1,1 +1,37 @@
 print("=========Identity Operators===========")
+
+'''The identity operators compare the objects to determine whether they share the same memory
+and refer to the same object type (data type).'''
+
+a = [1,2,3,4,5,6]
+b = [1,2,3,4,5,6]
+c = a
+print(a is b, id(a))
+print(a == b, id(b))
+print(a is c, id(c))
+
+element = None
+
+print(element is None, id(element))
+print(element == None,id(element))
+
+str = "abc"
+str1 = "abc"
+
+print(str is str1,id(str))
+print(str == str1,id(str1))
+
+a = 10
+b = 10
+c = b
+
+print(a is b)
+print(a is c)
+
+list1 = [1, 2, 3]
+list2 = list1          # reference (same object)
+list3 = list1.copy()   # naya object (alag memory)
+
+print(list1 is list2)   # True
+print(list1 is list3)   # False
+print(list1 == list3)   # True (values same hain)
