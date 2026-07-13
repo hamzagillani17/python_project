43d9729253f7fae1d4732362cbdc2081089468be
hg_python <hamzagillani17@gmail.com>
2026-07-13 15:39:15 +0200

    done

--- 05_Input_Data/input_function.py
+++ 05_Input_Data/input_function.py
@@ -1,1 +1,28 @@
 print("======= Input Function =======")
+
+name = input("Enter Your Name: ")
+age = input("Enter Your age Please: ")
+
+print("\nThanks")
+
+print("yor name is: ",name ,"\n", "Your are ",age," Year old")
+
+print(city := input("City: "))
+
+#Taking Numeric Input in Python
+width = input("Enter width : ")
+w= int(width)
+height = input("Enter height : ")
+h= int(height)
+area = w*h
+print ("Area of rectangle = ", area)
+
+#! /usr/bin/python3.11
+
+city="Lahore"
+state="punjab"
+country="Pakistan"
+print(city, state, country, sep=',')
+
+print("City:", city, end=" ")
+print("State:", state)
