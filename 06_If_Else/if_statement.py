4e4566c43e3b2af2631bb1bad3809a8880476979
hg_python <hamzagillani17@gmail.com>
2026-07-14 17:09:41 +0200

    strt

---
+++ 06_If_Else/if_statement.py
@@ -0,0 +1,25 @@
+print("\n=====Python If Statement=====")
+
+'''The if statement in Python evaluates whether a condition is true or false.
+It contains a logical expression that compares data, and a decision is made based on the result 
+of the comparison.'''
+
+a = 33
+b = 200
+if b > a:
+  print("b is greater than a")
+
+  if b < a: print("b is less than a") #it will not print anything because the condition is false
+
+  if(b == a):
+    print("b is equal to a") #it will not print anything because the condition is false
+
+    str = "Hello World"
+    str1 = "Hello World"
+    if(str == str1):
+      print("str is equal to str1") #it will print because the condition is true
+
+    if(10 != 10):
+      print("b is not equal to a") #it will print because the condition is true  
+
+      
