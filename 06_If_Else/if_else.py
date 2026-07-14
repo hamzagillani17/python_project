6203acaf0d80aff9685fefbc2757ebecd8d94f3d
hg_python <hamzagillani17@gmail.com>
2026-07-14 21:58:15 +0200

    strt

---
+++ 06_If_Else/if_else.py
@@ -0,0 +1,11 @@
+print("\n======if else======")
+
+'''The if-else statement in Python is used to execute a block of code 
+when the condition in the if statement is true,
+and another block of code when the condition is false.'''
+
+vote_age = int(input("Enter your age: "))
+if vote_age >= 18:
+    print("You are eligible to vote.")
+else:
+    print("You are not eligible to vote.")    
