e24ab594c9f13d581ffc46d7871f8b95f2814ab2
hg_python <hamzagillani17@gmail.com>
2026-07-15 13:47:17 +0200

    start

---
+++ 06_If_Else/match_case.py
@@ -0,0 +1,11 @@
+print("=======Match Case=======")
+'''Match Case : It is a new feature introduced in Python 3.10 that allows you to match values against patterns 
+   and execute code based on the matched pattern.
+   The syntax for a match case statement is:
+match value:
+    case pattern1:
+        # code to execute if value matches pattern1
+    case pattern2:
+        # code to execute if value matches pattern2
+    case _:
+        # code to execute if value does not match any pattern'''
